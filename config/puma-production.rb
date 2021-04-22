f9941b0d67ec8adbfa7502b57c7060d062ba522e
Chris Hall <chall8908@gmail.com>
2021-04-22 12:08:51 -0500

    Only bind to sockets given to us by systemd in production

--- config/puma-production.rb
+++ config/puma-production.rb
@@ -10,7 +10,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-bind 'unix:///run/xivrp/puma.sock'
+bind_to_activated_sockets 'only'
 pidfile '/run/xivrp/puma.pid'
 state_path '/run/xivrp/puma.state'
 
