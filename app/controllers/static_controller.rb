edba08192c3e693868127a69043a656c84f65994
Chris Hall <chall8908@gmail.com>
2021-04-25 20:02:25 -0500

    Add some legal stuff

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,12 @@ class StaticController < ApplicationController
   def about
     @compact_view = true
   end
+
+  def terms_of_service
+    @compact_view = true
+  end
+
+  def privacy_policy
+    @compact_view = true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'events#index'
 
   get :about, to: 'static#about'
+  get 'terms-of-service', to: 'static#terms_of_service'
+  get 'privacy-policy', to: 'static#privacy_policy'
 
   resources :events, param: :slug
 
