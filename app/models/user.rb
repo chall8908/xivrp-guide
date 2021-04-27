6e4b10227c09fa25c885daf360c84ec57587ba57
Chris Hall <chall8908@gmail.com>
2021-04-26 22:25:29 -0500

    Allow user with non-discord account to automatically link with discord

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,29 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token
+    # Allow either provider and UID or email with no existing provider
+    # info.
+    user = where(provider: auth.provider, uid: auth.uid)
+      .or(where(email: auth.info.email, provider: nil, uid: nil))
+      .first_or_initialize
 
+    user.provider ||= auth.provider
+    user.uid ||= auth.uid
+    user.email ||= auth.info.email
+
+    user.password = Devise.friendly_token unless user.encrypted_password?
+
+    user.skip_confirmation! unless user.confirmed?
+
+    unless user.avatar.present?
       url = URI.parse(auth.info.image)
       filename = File.basename(url.path)
 
       user.avatar.attach(io: URI.open(url), filename: filename)
-
-      user.skip_confirmation!
     end
+
+    user.save if user.has_changes_to_save?
+
+    user
   end
 end
